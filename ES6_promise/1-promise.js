8becede1303ed7055d8d4e5d1e8e25cf8dc4853d
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-10-09 17:22:30 -0500

    add new file

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -4,6 +4,6 @@ export default function getFullResponseFromAPI(success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
-      }
+    }
   });
 }
