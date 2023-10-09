9cbbb6370d77a5adeed00ecf88fc4af1874439ba
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-10-09 18:24:48 -0500

    add new file

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function getStudentsByLocation(array, city, grad) {
   return array
     .filter((i) => i.location === city)
     .map((student) => {
-     const gradeI = grad
+      const gradeI = grad
         .filter((i) => i.studentId === student.id)
         .map((x) => x.grade)[0];
-     const grade = gradeI || 'N/A';
-     return { ...student, grade };
+      const grade = gradeI || 'N/A';
+      return { ...student, grade };
     });
 }
